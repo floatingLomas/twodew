c0a365cdd0a11988f5f3d2686845e7ae53b05611
Jonathan Lomas <jonathan@floatinglomas.ca>
2014-05-14 23:47:14 -0700

    Added DELETE

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ module.exports = (function () {
         if (err) throw err;
 
         // Get a specific Todo by (Object)ID
-        app.get('/todos/:id', function (req, res) {
+        app.get('/todos/:id', function (req, res, next) {
             todos.findOne({
                 _id: new Todos.ObjectId(req.params.id)
             }, function (err, result) {
@@ -27,21 +27,21 @@ module.exports = (function () {
                     message: "Not found"
                 });
 
-                res.json(result);
+                res.json(addUri(result));
             });
         });
 
         // Get a list of Todos
-        app.get('/todos', function (req, res) {
+        app.get('/todos', function (req, res, next) {
             todos.find({}).toArray(function (err, result) {
                 if (err) return next(err);
 
-                return res.json(result);
+                return res.json(addUris(result));
             });
         });
 
         // Create a new Todo
-        app.post('/todos', validatePostedTodo, function (req, res) {
+        app.post('/todos', validatePostedTodo, function (req, res, next) {
             var posted = req.body;
 
             var todo = {
@@ -57,20 +57,46 @@ module.exports = (function () {
 
                 todo.done = false;
                 todo._type = 'todo';
-                todo._id = new Todos.ObjectId();
-                todo._uri = req.protocol + '://' + req.host + '/api/todos/' + todo._id;
 
                 todos.insert(todo, {
                     w: 1
                 }, function (err, saved) {
                     if (err) return next(err);
 
-                    if (!saved) return res.json()
-
-                    return res.json(201, saved);
+                    return res.json(201, addUri(saved[0]));
                 });
             });
         });
+
+        app.delete('/todos/:id', function (req, res, next) {
+            var objectId = new Todos.ObjectId(req.params.id);
+
+            if (!objectId) return res.json(404, {
+                message: "ID Not Found: " + objectId
+            });
+
+            todos.remove({
+                _id: objectId
+            }, function (err, result) {
+                if (err) return next(err);
+
+                if (result < 1) return res.json(404, {
+                    message: "Not found"
+                });
+
+                res.json(204);
+            });
+
+        });
+
+        function addUris(todos) {
+            return [].concat(todos || []).map(addUri);
+        }
+
+        function addUri(todo) {
+            if (todo && todo._id) todo._uri = 'http://localhost:5000/api/todos/' + todo._id;
+            return todo;
+        }
 
         function validatePostedTodo(req, res, next) {
             var posted = req.body;
