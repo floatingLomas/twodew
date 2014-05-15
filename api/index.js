c294a9db4a27d0aeab6e5dfed426a155902acdbb
Jonathan Lomas <jonathan@floatinglomas.ca>
2014-05-15 10:49:31 -0700

    Refactored out a Todos service to get, find, save (and soon delete)

--- api/index.js
+++ api/index.js
@@ -7,69 +7,62 @@ module.exports = (function () {
     var express = require('express');
     var bodyParser = require('body-parser');
 
-    var app = express.Router();
+    var router = express.Router();
 
+    router.use(bodyParser.json());
+
+    var db = require('./db');
     var Todos = require('./todos');
 
-    app.use(bodyParser.json());
-
-    Todos.connect(function (err, todos) {
+    db.connect(function (err, collection) {
         if (err) throw err;
 
+        var todos = new Todos(collection);
+
         // Get a specific Todo by (Object)ID
-        app.get('/todos/:id', function (req, res, next) {
-            todos.findOne({
-                _id: new Todos.ObjectId(req.params.id)
-            }, function (err, result) {
+        router.get('/todos/:id', function (req, res, next) {
+            todos.get(req.params.id, function (err, todo) {
                 if (err) return next(err);
 
-                if (!result) return res.json(404, {
-                    message: "Not found"
-                });
+                if (!todo) return res.json(404);
 
-                res.json(addUri(result));
+                return res.json(todo);
             });
         });
 
-        // Get a list of Todos
-        app.get('/todos', function (req, res, next) {
-            todos.find({}).toArray(function (err, result) {
+        // Get a list of db
+        router.get('/todos', function (req, res, next) {
+            todos.find(req.query).toArray(function (err, result) {
                 if (err) return next(err);
 
-                return res.json(addUris(result));
+                return res.json(result);
             });
         });
 
         // Create a new Todo
-        app.post('/todos', validatePostedTodo, function (req, res, next) {
+        router.post('/todos', validatePostedTodo, function (req, res, next) {
             var posted = req.body;
 
             var todo = {
                 title: posted.title,
-                body: posted.body
+                body: posted.body,
+                done: false
             };
 
-            todos.findOne(todo, function (err, result) {
+            todos.save(todo, function (err, saved) {
                 if (err) return next(err);
-                if (result) return res.json(409, {
+
+                if (!saved) return res.json(409, {
                     message: "Record already exists"
                 });
 
-                todo.done = false;
-                todo._type = 'todo';
-
-                todos.insert(todo, {
-                    w: 1
-                }, function (err, saved) {
-                    if (err) return next(err);
-
-                    return res.json(201, addUri(saved[0]));
-                });
+                return res.json(201, saved);
             });
         });
 
-        app.delete('/todos/:id', function (req, res, next) {
-            var objectId = new Todos.ObjectId(req.params.id);
+        // Kill a Todo
+        router.delete('/todos/:id', function (req, res, next) {
+            var objectId = new db.ObjectId(req.params.id);
 
             if (!objectId) return res.json(404, {
                 message: "ID Not Found: " + objectId
@@ -88,15 +81,6 @@ module.exports = (function () {
             });
 
         });
-
-        function addUris(todos) {
-            return [].concat(todos || []).map(addUri);
-        }
-
-        function addUri(todo) {
-            if (todo && todo._id) todo._uri = 'http://localhost:5000/api/todos/' + todo._id;
-            return todo;
-        }
 
         function validatePostedTodo(req, res, next) {
             var posted = req.body;
@@ -125,5 +109,5 @@ module.exports = (function () {
         }
     });
 
-    return app;
+    return router;
 })();
--- api/todos.js
+++ api/todos.js
@@ -1,53 +1,122 @@
 'use strict';
 
 /**
- * Set up the connection to the MongoDB 'Todos' collection
+ * Expose Todos service
  */
-module.exports = (function Todos() {
-    var mongo = require('mongodb');
-    var mongoUri = process.env.MONGOHQ_URI || 'mongodb://localhost/TwoDew';
 
-    var _db = null;
-    var _collection = null;
+module.exports = Todos;
 
-    function connect(next) {
-        mongo.Db.connect(mongoUri, function (err, db) {
-            if (err) return next(err);
-            _db = db;
+function Todos(collection) {
+    if (!collection) throw new Error('MongoDB Collection is required.');
 
-            console.log('Connected to MongoDB at ' + mongoUri);
+    this._collection = collection;
 
-            db.collection('Todos', function (err, collection) {
-                if (err) return next(err);
-                _collection = collection;
+    return this;
+};
 
-                console.log('Found Todos collection');
+/**
+ *  Get a single Todo by its ID (or a null if not found)
+ *  passed into the callback.
+ *
+ *  @param {Number} id
+ *  @param {Function} next
+ *  @api public
+ */
+Todos.prototype.get = function (id, next) {
+    this._collection.findOne({
+        _id: id
+    }, next);
+};
 
-                return collection.ensureIndex({
-                    title: 'text',
-                    subject: 'text'
-                }, {
-                    name: 'todo_text_index',
-                    weight: {
-                        title: 10,
-                        subject: 5
-                    },
-                    background: true
-                }, function (err, indexName) {
-                    if (err) return next(err);
+/**
+ * Get Todos given the criteria.  If you provide a callback, it will run; if you do not,
+ * it will return Mongo's chainable Cursor.
+ *
+ * Criteria is an object which can contain none, some or all of these keys:
+ *
+ *      done    {Boolean}   done or not
+ *      text    {String}    string to search for in both title and body (via regex)
+ *      title   {String}    string to search for in the title (via regex)
+ *      body    {String}    string to search for in the body (via regex)
+ *      case    {Boolean}   case-sensitive regexes (default) or not
+ *
+ * @param {Object} criteria
+ * @param {Function} next
+ * @return {Object} Cursor
+ * @api public
+ */
+Todos.prototype.find = function (criteria, next) {
+    var cursor = this._collection.find(parseCriteria(criteria));
 
-                    console.log('Indices in place');
+    if (next) cursor = cursor.toArray(next);
 
-                    return next(null, collection);
-                });
-            });
-        });
+    return cursor;
+};
+
+/**
+ * Save a Todo.  You'll get an error if the title or body are not valid.
+ *
+ * Callback will get any errors (or null), and the inserted Todo
+ * (or null if it is a duplicate title).
+ *
+ * @param {Todo} todo
+ * @param {Function} next
+ * @api public
+ */
+Todos.prototype.save = function (todo, next) {
+    if (!todo) return next(new Error("Todo was undefined"));
+
+    if (!todo.title || typeof todo.body !== 'string') {
+        return next(new Error("Bad Todo title: " + JSON.stringify(todo.title)));
+    }
+    if (!todo.body || typeof todo.body !== 'string') {
+        return next(new Error("Bad Todo body: " + JSON.stringify(todo.body)));
     }
 
-    return {
-        connect: connect,
-        ObjectId: mongo.ObjectID,
-        db: _db,
-        collection: _collection
+    var _todo = {
+        title: todo.title,
+        body: todo.body,
+        done: false
+    };
+
+    this._collection.insert(_todo, {
+        w: 1
+    }, function (err, result) {
+        // If this is a duplicate key error, don't pass it along
+        if (err && err.code == 11000) err = null;
+
+        result = (!result) ? null : result[0];
+
+        return next(err, result);
+    });
+}
+
+function parseCriteria(crit) {
+    var search = {};
+
+    if (!crit) return search;
+
+    if (~['false', 'true'].indexOf(crit.done)) search.done = crit.done === 'true';
+
+    var caseSensitive = !crit['case-sensitive'];
+
+    if (crit.title) search.title = assembleCriterion(crit.title, caseSensitive);
+    if (crit.body) search.body = assembleCriterion(crit.body, caseSensitive);
+
+    if (crit.text) {
+        search.$or = [{
+            title: assembleCriterion(crit.text, caseSensitive)
+        }, {
+            body: assembleCriterion(crit.text, caseSensitive)
+        }];
     }
-})();
+
+    function assembleCriterion(value, caseSensitive) {
+        return {
+            $regex: value,
+            $options: (caseSensitive) ? 'i' : ''
+        };
+    }
+
+    return search;
+}
